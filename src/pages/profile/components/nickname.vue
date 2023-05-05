24512b462f55c476f9775dd3b46f35248ac9bcfa
feiandxs <yiziyint@gmail.com>
2023-05-05 23:12:39 +0800

    ts

---
+++ src/pages/profile/components/nickname.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Date: 2023-05-05 23:11:11
+ * @LastEditors: ibegyourpardon
+ * @LastEditTime: 2023-05-05 23:11:57
+ * @FilePath: /my-uniapp-demo-vite-ts/src/pages/profile/components/nickname.vue
+-->
+<template>
+  <view>
+    <text>nickname: {{ nickname }}</text>
+  </view>
+</template>
+<script setup lang="ts">
+/* __vue_virtual_code_placeholder__ */
+import type { PropType } from 'vue';
+const props = defineProps({
+  nickname: {
+    type: String as PropType<string>,
+    default: 'default nickname',
+  },
+});
+
+</script>
--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,19 +1,25 @@
 <!--
  * @Date: 2023-04-27 15:20:34
  * @LastEditors: ibegyourpardon
- * @LastEditTime: 2023-04-27 15:22:03
- * @FilePath: \my-uniapp-demo-vite-ts\src\pages\profile\index.vue
+ * @LastEditTime: 2023-05-05 23:12:13
+ * @FilePath: /my-uniapp-demo-vite-ts/src/pages/profile/index.vue
 -->
 <template>
   <view>
     profile index
     <view>
       {{ title }} </view>
+    <NickName
+      nickname="acbc"
+    />
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+
+import NickName from './components/nickname.vue';
+
 const title = ref('Profile');
 
 </script>
