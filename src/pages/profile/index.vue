871777e925ae193a48fb00fa5f1d0d786e14f08d
feiandxs <yiziyint@gmail.com>
2023-05-05 19:05:20 +0800

    x

---
+++ src/pages/profile/index.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Date: 2023-04-27 15:20:34
+ * @LastEditors: ibegyourpardon
+ * @LastEditTime: 2023-04-27 15:22:03
+ * @FilePath: \my-uniapp-demo-vite-ts\src\pages\profile\index.vue
+-->
+<template>
+  <view>
+    profile index
+    <view>
+      {{ title }} </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const title = ref('Profile');
+
+</script>
